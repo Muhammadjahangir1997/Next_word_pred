e0ddd0d5e17545567d69c14f3717d7186014434f
Muhammad _Jahangir <159331292+Muhammadjahangir1997@users.noreply.github.com>
2026-02-12 21:22:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,3 @@
-# import streamlit as st
-# import requests
-
-# st.set_page_config(page_title="Next Word Prediction", page_icon="🤖")
-
-# st.title("Next Word Prediction System")
-# st.write("Enter text below and click Predict Next Words")
-
-# # --------------------
-# # User Input
-# # --------------------
-# user_text = st.text_input("Enter starting text")
-# n_words = st.number_input("Number of words to predict", min_value=1, max_value=20, value=5)
-
-# # --------------------
-# # Predict Button
-# # --------------------
-# if st.button("Predict Next Words"):
-#     if user_text.strip() == "":
-#         st.warning("Please enter some text")
-#     else:
-#         try:
-#             response = requests.post(
-#                 "http://127.0.0.1:8000/predict",
-#                 json={"text": user_text, "n_words": n_words}
-#             )
-
-#             if response.status_code == 200:
-#                 result = response.json()
-#                 st.success(f"Predicted Text: {result['predicted_text']}")
-#             else:
-#                 st.error("API Error: Unable to get prediction")
-
-#         except Exception as e:
-#             st.error(f"Connection Error: {e}")
 import streamlit as st
 import pickle
 import numpy as np
@@ -80,3 +45,4 @@ if st.button("Predict"):
     else:
         result = predict_text(user_text, n_words)
         st.success(f"Predicted Text: {result}")
+
